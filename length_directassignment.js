6494f969da0f535b4dfaa79a614edc5f14fd649c
Rex Zulfekar <rzulfekar@users.noreply.github.com>
2024-02-25 13:13:46 -0500

    print length

--- length_directassignment.js
+++ length_directassignment.js
@@ -1,4 +1,4 @@
 const pets = ['dog', 'cat', 'rabbit', 'hamster'];
 //Use the length property to determine how many objects are in the array
-
+console.log('length: ', pets.length);
 //Using direct assignment, add 'snake' to the array in index position 2.
